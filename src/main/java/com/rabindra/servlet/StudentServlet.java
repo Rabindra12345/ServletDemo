84153b42e338a127776305370a47dc09cd038e34
rabindra <rabindra@red27mobile.com>
2023-05-02 08:52:24 +0545

    added html form and sent request to servlet

---
+++ src/main/java/com/rabindra/servlet/StudentServlet.java
@@ -0,0 +1,58 @@
+package com.rabindra.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//set the content type
+		response.setContentType("text/html");
+		
+		//get the print writer
+		PrintWriter writer = response.getWriter();
+		
+		//display the html content
+		writer.println("<html><body>");
+		
+		writer.println("<h4>Student is confirmed: <h4>"
+		+request.getParameter("firstName")+" "
+		+request.getParameter("lastName")
+				);
+
+		
+		writer.println("</html></body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
